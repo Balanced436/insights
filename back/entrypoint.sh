b26634ac32678716544c08116c559b26269f96ac
SAID <ismael.saidpro@protonmail.com>
2024-12-01 20:43:50 +0100

    [FEATURE] Upgrade Node.js version and enhance database setup for testing environment

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -4,19 +4,27 @@ POSTGRES_USER=$(cat /run/secrets/postgres_user)
 POSTGRES_PASSWORD=$(cat /run/secrets/postgres_password)
 POSTGRES_DB=$(cat /run/secrets/postgres_db)
 DATABASE_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/${POSTGRES_DB}?schema=public"
-
-## Write the databse_url string in .env file
 echo "DATABASE_URL=$DATABASE_URL" >> .env
 
-# Vérifier si l'application est en mode développement
+POSTGRES_USER_TEST="${POSTGRES_USER}"
+POSTGRES_PASSWORD_TEST="${POSTGRES_PASSWORD}"
+POSTGRES_DB_TEST="${POSTGRES_DB}_test"
+DATABASE_URL_TEST="postgresql://${POSTGRES_USER_TEST}:${POSTGRES_PASSWORD_TEST}@${POSTGRES_HOST}:5432/${POSTGRES_DB_TEST}?schema=public"
+echo "DATABASE_URL=$DATABASE_URL_TEST" >> .env.test
+
 if [ "$NODE_ENV" = "development" ]; then
-    echo "Lancement de prisma"
-    npm run reset &
-    npm run push &
-    npm run seed &
-    npx prisma studio &
-    echo "Lancement en mode dev"
+    echo "Lancement de prisma en mode dev"
+    npm run reset
+    npm run push
+    npm run seed
     npm run dev
+    npx prisma studio &
+
+    echo "Lancement de prisma en mode test"
+    npm run reset:test
+    npm run push:test
+    npm run seed:test
+
 else
     echo "Lancement en mode prod"
     npm start
