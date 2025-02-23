29368adc0eb411b9660511682079c9f6b5d60bb5
SAID <ismael.saidpro@protonmail.com>
2025-02-23 13:43:25 +0100

    [FEATURE] Update seed script to handle multiple sources

--- back/prisma/seed.ts
+++ back/prisma/seed.ts
@@ -3,19 +3,33 @@ import app from '../src/app'
 import path from 'path';
 
 async function main() {
-    const newSource = {
-        title: 'SEED title JT 5-11-2024-19h',
+    const newSource = [{
+        title: 'SEED1 title JT 5-11-2024-19h',
         description: 'SEED description JT 5-11-2024-19h',
         video: path.resolve(__dirname, '../src/__tests__/fixtures/5-11-2024-19h.mp4'),
         audio: path.resolve(__dirname, '../src/__tests__/fixtures/5-11-2024-19h.wav')
-      };
+      },{
+        title: 'SEED2 title JT 5-11-2024-19h',
+        description: 'SEED description JT 5-11-2024-19h',
+        video: path.resolve(__dirname, '../src/__tests__/fixtures/5-11-2024-19h.mp4'),
+        audio: path.resolve(__dirname, '../src/__tests__/fixtures/5-11-2024-19h.wav')
+      },{
+        title: 'SEED3 title JT 5-11-2024-19h',
+        description: 'SEED description JT 5-11-2024-19h',
+        video: path.resolve(__dirname, '../src/__tests__/fixtures/5-11-2024-19h.mp4'),
+        audio: path.resolve(__dirname, '../src/__tests__/fixtures/5-11-2024-19h.wav')
+      }];
 
-      await request(app)
-      .post('/source')
-      .field('title', newSource.title)
-      .field('description', newSource.description)
-      .attach('video', newSource.audio)
-      .attach('audio', newSource.video);
+  
+
+      for(var element of newSource){
+        await request(app)
+        .post('/source')
+        .field('title', element.title)
+        .field('description', element.description)
+        .attach('video', element.audio)
+        .attach('audio', element.video);
+      }
     }
 
 main()
