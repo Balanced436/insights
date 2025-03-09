9012489ac2d9647c152f60622beff9105c9fe914
SAID <ismael.saidpro@protonmail.com>
2025-03-09 18:31:19 +0100

    [CHORE] add socket io as dependency and clear repo

--- front/src/__tests__/fixtures/Source.test.tsx
+++
@@ -1,6 +0,0 @@
-import Source from "../../components/Sources"
-import {render, screen} from '@testing-library/react'
-it("Should display no sources ",async ()=>{
-    render(<Source sources={[]}/>)
-    screen.getByText('No sources')
-})
