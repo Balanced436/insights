1c29edaa1bb66444fde1909f45186b90abcd235f
SAID <ismael.saidpro@protonmail.com>
2025-03-11 18:46:40 +0100

    [FEATURE] Refactor transcription handling to create tasks and manage statuses based on skipTranscription flag

--- back/src/routes/transcription.ts
+++ back/src/routes/transcription.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Source, TaskType, Transcription } from "@prisma/client";
+import { PrismaClient, Source, Status, Task, TaskType, Transcription } from "@prisma/client";
 import { Request, Response, Router } from "express";
 import { uniqueId } from "lodash";
 import fs from "fs";
@@ -23,7 +23,7 @@ const TranscriptionRouter = (io: Server) => {
             id: id
           }
         });
-        return res.status(200).json({ data: transcription });
+        return res.status(200).json({ data: transcription || {} });
       }
     } catch (error: any) {
       return res.status(400).json({ error: "Internal Server Error", details: error.message });
@@ -55,39 +55,73 @@ const TranscriptionRouter = (io: Server) => {
         }
       });
 
-      // open audio file associated with the source and call whisper to restranscript it
-      fs.openAsBlob(audioFile).then((audioFile: Blob) => {
-        const formData = new FormData();
-        formData.append("file", audioFile, "file.wav");
-        if(!skipTranscription){
-        fetch("http://whisper:8080/inference", {
-          method: "POST",
-          body: formData
-        })
-          .then(resp => resp.ok ? resp.json() : Promise.reject(resp))
-          .then(res => {
-            // use the response to update this transcription
-            fetch(`http://localhost:4000/transcription/${transcription.id}`, {
-              method: "PUT", 'headers': {
-                'Content-Type': 'application/json'
-              }, body: JSON.stringify({ content: JSON.stringify(res) })
-            }).then((resp => resp.ok ? resp.json() : Promise.reject(resp))).then(res => console.log("transcription ended")).catch(error => error)
-          })
-          .catch(error => {
+      const task: Task = await prisma.task.create({ data: { transcriptionId: transcription.id, type: TaskType.TRANSCRIPTION } });
+
+      if (!skipTranscription) {
+        const processTranscription = async () => {
+          try {
+            const audioFileBlob = await fs.promises.readFile(audioFile);
+            const formData = new FormData();
+            formData.append("file", new Blob([audioFileBlob]), "file.wav");
+
+            const response = await fetch("http://whisper:8080/inference", {
+              method: "POST",
+              body: formData
+            });
+            if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const res = await response.json();
+            await prisma.transcription.update({
+              where: { id: transcription.id },
+              data: { content: JSON.stringify(res) }
+            });
+            await prisma.task.update({
+              where: { id: task.id },
+              data: { status: Status.COMPLETED, finishedAt: new Date() }
+            });
+          } catch (error) {
+            // TODO catch this 
+                        /* 2025-03-11 11:21:59 backend-1       | GET /task {}
+            2025-03-11 11:22:56 backend-1       | Fetch error: TypeError: fetch failed
+            2025-03-11 11:22:56 backend-1       |     at Object.fetch (node:internal/deps/undici/undici:11457:11) {
+            2025-03-11 11:22:56 backend-1       |   cause: HeadersTimeoutError: Headers Timeout Error
+            2025-03-11 11:22:56 backend-1       |       at Timeout.onParserTimeout [as callback] (node:internal/deps/undici/undici:9647:32)
+            2025-03-11 11:22:56 backend-1       |       at Timeout.onTimeout [as _onTimeout] (node:internal/deps/undici/undici:7948:17)
+            2025-03-11 11:22:56 backend-1       |       at listOnTimeout (node:internal/timers:573:17)
+            2025-03-11 11:22:56 backend-1       |       at processTimers (node:internal/timers:514:7) {
+            2025-03-11 11:22:56 backend-1       |     code: 'UND_ERR_HEADERS_TIMEOUT'
+            2025-03-11 11:22:56 backend-1       |   }
+            2025-03-11 11:22:56 backend-1       | } */
             console.error("Fetch error:", error);
+            await prisma.task.update({
+              where: { id: task.id },
+              data: { status: Status.ERROR, finishedAt: new Date() }
+            });
+            
+
             throw error;
-          })}else{
-            fetch(`http://localhost:4000/transcription/${transcription.id}`, {
-              method: "PUT", 'headers': {
-                'Content-Type': 'application/json'
-              }, body: JSON.stringify({ content: "The flag skip transcription is set to true" })
-            }).then((resp => resp.ok ? resp.json() : Promise.reject(resp))).then(res => res).catch(error => error)
           }
-      })
-      const task  = {type : TaskType.TRANSCRIPTION, transcriptionId : transcription.id}
-      const taskId = await prisma.task.create({data : task})
-      
-      return res.status(201).json({ data: { sourceId: transcription.sourceId, taskId: taskId } });
+        };
+        processTranscription();
+      } else {
+        const fakeTranscription = async () => {
+          console.info("skipTranscription is set to true");
+          console.info("transcription.id", transcription.id);
+          console.info("task.id", task.id);
+          await prisma.transcription.update({
+            where: { id: transcription.id },
+            data: { content: "The flag skip transcription is set to true" }
+          });
+          await prisma.task.update({
+            where: { id: task.id },
+            data: { status: Status.COMPLETED, finishedAt: new Date() }
+          });
+        };
+        fakeTranscription();
+      }
+      return res.status(201).json({ task: task });
+
 
     } catch (error: any) {
       return res.status(400).json({ error: "Internal Server Error", details: error.message });
@@ -111,8 +145,6 @@ const TranscriptionRouter = (io: Server) => {
     } catch (error: any) {
       return res.status(400).json({ error: "Internal Server Error", details: error.message });
     }
-
-
   });
 
   router.delete("/transcription/:id?", async (req: Request, res: Response): Promise<any> => {
