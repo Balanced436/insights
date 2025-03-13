636a50e67a4fb27053acfcb98c32cb9d887a0da4
SAID <ismael.saidpro@protonmail.com>
2025-03-13 10:45:32 +0100

    [FEATURE] Add seed data for sources and transcriptions in the database seeding script

--- back/prisma/seed.ts
+++ back/prisma/seed.ts
@@ -3,7 +3,8 @@ import app from '../src/app'
 import path from 'path';
 
 async function main() {
-    const newSource = [{
+    // add some sources
+    const sources = [{
         title: 'SEED1 title JT 5-11-2024-19h',
         description: 'SEED description JT 5-11-2024-19h',
         video: path.resolve(__dirname, '../src/__tests__/fixtures/5-11-2024-19h.mp4'),
@@ -20,15 +21,23 @@ async function main() {
         audio: path.resolve(__dirname, '../src/__tests__/fixtures/5-11-2024-19h.wav')
       }];
 
-  
+      // add some transcriptions
+      const transcriptions = [
+        {"sourceId":1, "skipTranscription": true},
+        {"sourceId":2, "skipTranscription": true},
+        {"sourceId":3, "skipTranscription": true}]
 
-      for(var element of newSource){
+      for(var element of sources){
         await request(app)
         .post('/source')
         .field('title', element.title)
         .field('description', element.description)
         .attach('video', element.video)
         .attach('audio', element.audio);
+      }
+
+      for (var transcription of transcriptions){
+        await request(app).post('/transcription').send({"sourceId": transcription.sourceId, "skipTranscription":transcription.skipTranscription})
       }
     }
 
