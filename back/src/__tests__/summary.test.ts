3cd65e85010436b80208dee54b4e4ab97f209c91
SAID <ismael.saidpro@protonmail.com>
2025-03-14 20:32:21 +0100

    [FEATURE] Enhance summary tests with CRUD operations and add data seeding for sources and transcriptions

--- back/src/__tests__/summary.test.ts
+++ back/src/__tests__/summary.test.ts
@@ -1,7 +1,67 @@
+import path from "path";
+import request from "supertest";
+import app from "../app";
+import { Prisma, PrismaClient } from "@prisma/client";
 describe("CRUD operations from summary", () => {
-    describe("GET /summary", () => {
-        it("should return all summaries and stauts 200", () => {
+    const prisma = new PrismaClient();
+    
+    const sources = [{
+        title: 'SEED1 title JT 5-11-2024-19h',
+        description: 'SEED description JT 5-11-2024-19h',
+        video: path.resolve(__dirname, "fixtures/5-11-2024-19h.mp4"),
+        audio: path.resolve(__dirname, "fixtures/5-11-2024-19h.wav"),
+    }, {
+        title: 'SEED2 title JT 5-11-2024-19h',
+        description: 'SEED description JT 5-11-2024-19h',
+        video: path.resolve(__dirname, "fixtures/5-11-2024-19h.mp4"),
+        audio: path.resolve(__dirname, "fixtures/5-11-2024-19h.wav"),
+    }, {
+        title: 'SEED3 title JT 5-11-2024-19h',
+        description: 'SEED description JT 5-11-2024-19h',
+        video: path.resolve(__dirname, "fixtures/5-11-2024-19h.mp4"),
+        audio: path.resolve(__dirname, "fixtures/5-11-2024-19h.wav"),
+    }];
+
+    // add some transcriptions
+    const transcriptions = [
+        { "sourceId": 1, "skipTranscription": true },
+        { "sourceId": 2, "skipTranscription": true },
+        { "sourceId": 3, "skipTranscription": true }]
+
+    beforeAll(async () => {
+        for (var element of sources) {
+            await request(app)
+                .post('/source')
+                .field('title', element.title)
+                .field('description', element.description)
+                .attach('video', element.video)
+                .attach('audio', element.audio);
+        }
+
+        for (var transcription of transcriptions) {
+            await request(app).post('/transcription').send({ "sourceId": transcription.sourceId, "skipTranscription": transcription.skipTranscription })
+        }
+
+    })
+
+    describe("POST /summary/:transcriptionid?", () => {
+        it("should post a new summary and return 201 if id is provided and return the summary", () => {
+            const response = await request(app).post("/summary").then()
             expect(true).toBe(false)
+        })
+
+        it("should return status 400 if transcription id is not provided", () => {
+            expect(true).toBe(false)
+        })
+    })
+
+    describe("GET /summary", () => {
+        it("should return all summaries and stauts 200", async () => {
+            const summaries = await prisma.summary.findMany()
+            const response = await request(app).get("/summary") 
+            
+            expect(response.body.length).toBe(summaries.length)
+            expect(response.status).toBe(200)
         })
 
         it("should return a summary by id", () => {
@@ -9,16 +69,6 @@ describe("CRUD operations from summary", () => {
         })
 
         it("should return 400 if id is nan", () => {
-            expect(true).toBe(false)
-        })
-    })
-
-    describe("POST /summary/:transcriptionid?", () => {
-        it("should post a new summary and return 201 if id is provided and return the summary", () => {
-            expect(true).toBe(false)
-        })
-
-        it("should return status 400 if transcription id is not provided", () => {
             expect(true).toBe(false)
         })
     })
