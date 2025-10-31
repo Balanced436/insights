dbe691bd9e28df2584f3e0b7789114ddaea5219a
SAID <ismael.saidpro@protonmail.com>
2025-10-31 15:49:07 +0400

    [FEATURE] test breadcrumbs, update corpusSearchbar

--- front/src/components/corpus/CorpusSearchBar.tsx
+++ front/src/components/corpus/CorpusSearchBar.tsx
@@ -1,33 +1,43 @@
-import { Box, IconButton, InputAdornment, Stack, TextField } from '@mui/material';
+import {Box, IconButton, InputAdornment, Stack, TextField, Button, Typography} from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import CloseIcon from '@mui/icons-material/Close';
+import {DatePicker, LocalizationProvider} from "@mui/x-date-pickers"
+import {AdapterDateFns} from '@mui/x-date-pickers/AdapterDateFns';
 
+
+type corpusSearchBarProps = {
+    onAddSourceClick: () => {},
+    onSearchBarChange: () => {},
+    onDateChange: () => {},
+}
 const CorpusSearchBar = () => {
-	return (
-		<Stack marginTop={'2em'} direction="row" justifyContent={'center'}>
-			<Box>
-				<TextField
-					placeholder={'Search a corpus'}
-					slotProps={{
-						input: {
-							startAdornment: (
-								<InputAdornment position="start">
-									<SearchIcon />
-								</InputAdornment>
-							),
-							endAdornment: (
-								<InputAdornment position="end">
-									<IconButton aria-label="AddIcon">
-										<CloseIcon />
-									</IconButton>
-								</InputAdornment>
-							),
-						},
-					}}
-				/>
-			</Box>
-		</Stack>
-	);
+    return (
+        <Stack marginY={'1em'} direction={"row"} justifyContent={'space-between'}>
+
+            <Stack direction={"row"}>
+                <TextField
+                    placeholder={'Search a corpus'}
+                    slotProps={{
+                        input: {
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <SearchIcon/>
+                                </InputAdornment>
+                            ),
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton aria-label="AddIcon">
+                                        <CloseIcon/>
+                                    </IconButton>
+                                </InputAdornment>
+                            ),
+                        },
+                    }}
+                />
+                <Button>Search</Button>
+            </Stack>
+        </Stack>
+    );
 };
 
 export default CorpusSearchBar;
--- front/src/pages/CorpusPage.tsx
+++ front/src/pages/CorpusPage.tsx
@@ -2,7 +2,9 @@ import {useNavigate, useParams} from '@tanstack/react-router';
 import { useSources } from '../hooks/useSources.ts';
 import SourcesGridView from '../components/source/SourcesGridView.tsx';
 import Source from '../models/source.ts';
-import {Box} from "@mui/material";
+import {Box, Typography, Button, IconButton, TextField, Breadcrumbs, Link, Divider, Stack} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import CorpusSearchBar from "../components/corpus/CorpusSearchBar.tsx";
 
 const CorpusInfosPage = () => {
 	const { corpusid } = useParams({ from: '/corpora/$corpusid/sources' });
@@ -27,10 +29,29 @@ const CorpusInfosPage = () => {
 	if (sources) {
 		return (
             <Box sx={{p:"1em"}}>
-                <SourcesGridView
-                    sources={sources}
-                    onSourceSelection={handleSourceSelection}
-                />
+				<Breadcrumbs aria-label="breadcrumb">
+					<Link underline="hover" color="inherit" href="/">
+						Corpora
+					</Link>
+					<Link underline="hover" color="inherit" href="/">
+						Sources
+					</Link>
+					<Typography sx={{ color: 'text.primary' }}>Dashboard</Typography>
+				</Breadcrumbs>
+				<CorpusSearchBar/>
+
+				<Box>
+					<Stack orientation={"row"} justifyContent={"space-between"}>
+							<Typography variant={"subtitle1"}>The content of this corpus</Typography>
+							<AddIcon>
+						</AddIcon>
+
+					</Stack>
+					<SourcesGridView
+						sources={sources}
+						onSourceSelection={handleSourceSelection}
+					/>
+				</Box>
 
             </Box>
 		);
